875c67c78d647854ac7be5aa9707f9ce882e4e5d
milkitic <milkitic@126.com>
2018-06-28 10:42:54 +0800

    某xml被吃

--- Sisters.WudiLib/Utilities.cs
+++ Sisters.WudiLib/Utilities.cs
@@ -120,6 +120,7 @@ namespace Sisters.WudiLib
         /// 检查<see cref="string"/>是否为<c>null</c>或空值，并抛出相应的异常。
         /// </summary>
         /// <param name="argument">要检查的<see cref="string"/>。</param>
+        /// <param name="paramName">TODO</param>
         /// <exception cref="ArgumentException"><c>argument</c>为空。</exception>
         /// <exception cref="ArgumentNullException"><c>argument</c>为<c>null</c>。</exception>
         internal static void CheckStringArgument(string argument, string paramName)
