7aab5ebed9e710ad7434a5310f96d97fe84ffab7
milkitic <milkitic@126.com>
2018-06-28 10:49:16 +0800

    改section命名

--- Sisters.WudiLib/SectionMessage.cs
+++ Sisters.WudiLib/SectionMessage.cs
@@ -5,14 +5,14 @@ namespace Sisters.WudiLib
 {
     public abstract partial class SectionMessage : Message
     {
-        protected SectionMessage() => sections = new List<Section>();
+        protected SectionMessage() => SectionsBase = new List<Section>();
 
         protected SectionMessage(IEnumerable<Section> sections)
-            => this.sections = new List<Section>(sections);
+            => this.SectionsBase = new List<Section>(sections);
 
-        protected readonly IList<Section> sections;
+        protected readonly IList<Section> SectionsBase;
 
-        public override string Raw => GetRaw(sections);
+        public override string Raw => GetRaw(SectionsBase);
 
         internal static string GetRaw(IEnumerable<Section> sections)
             => string.Join(string.Empty, sections.Select(section => section.Raw));
