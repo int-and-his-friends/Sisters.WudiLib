87ea061ecdc8ef2d2f9e6e791394b88202f484c4
bleatingsheep <bleatingsheep@live.cn>
2018-07-17 16:39:41 +0800

    增加固定消息功能。

--- Sisters.WudiLib/Posts/ReceivedMessage.cs
+++ Sisters.WudiLib/Posts/ReceivedMessage.cs
@@ -1,8 +1,10 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 using static Sisters.WudiLib.SectionMessage;
 
 namespace Sisters.WudiLib.Posts
@@ -53,6 +55,73 @@ namespace Sisters.WudiLib.Posts
         internal override object Serializing => this.Forward().Serializing;
 
         public override string Raw => _isString ? _message : GetRaw(_sections);
+
+        /// <summary>
+        /// 获取固定消息。可以将此字符串保存到本地，在任何时候发送时，都可以发送此消息，不用担心缓存被清或者文件过期。
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="AggregateException">多半是网络错误。</exception>
+        public string Fix()
+        {
+            if (_isString)
+            {
+                return Regex.Replace(_message, CqCodePattern, m =>
+                {
+                    if (m.Groups[1].Value == Section.ImageType)
+                    {
+                        for (int i = 0; i < m.Groups[2].Captures.Count; i++)
+                        {
+                            if (m.Groups[2].Captures[i].Value == "url")
+                            {
+                                string url = m.Groups[3].Captures[i].Value.AfterReceive();
+                                return GetFixedImageSection(url);
+                            }
+                        }
+                    }
+                    return m.Value;
+                });
+            }
+            else
+            {
+                var result = new StringBuilder();
+                foreach (var section in _sections)
+                {
+                    if (section.Type == Section.ImageType)
+                    {
+                        result.Append(section.Data.TryGetValue("url", out string url)
+                            ? GetFixedImageSection(url)
+                            : section.Raw);
+                    }
+                    else
+                    {
+                        result.Append(section.Raw);
+                    }
+                }
+                return result.ToString();
+            }
+        }
+
+        /// <summary>
+        /// 获取<c>url</c>指向图片的固定消息。
+        /// </summary>
+        /// <param name="url">指向图片的url。</param>
+        /// <returns>可以发送该图片的消息。</returns>
+        /// <exception cref="AggregateException">网络错误。</exception>
+        /// <exception cref="ArgumentNullException"><c>url</c>是<c>null</c>。</exception>
+        private static string GetFixedImageSection(string url)
+        {
+            if (url == null)
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            using (var http = new HttpClient())
+            {
+                var imageBytes = http.GetByteArrayAsync(url).Result;
+                var base64 = Convert.ToBase64String(imageBytes);
+                return $"[CQ:image,file=base64://{base64.BeforeSend()}]";
+            }
+        }
 
         public string Text
         {
