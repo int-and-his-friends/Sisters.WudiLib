3538ce7d45c8631f1319a9f66175dd9be88422ed
bleatingsheep <bleatingsheep@live.cn>
2018-12-28 23:38:21 +0800

    添加 Sisters.WudiLib.WebSocket.Test。

---
+++ Sisters.WudiLib.WebSocket.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sisters.WudiLib.WebSocket.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cqWebSocketEvent = new CqWebSocketEvent("");
+            var httpApiClient = new HttpApiClient();
+            cqWebSocketEvent.ApiClient = httpApiClient;
+            cqWebSocketEvent.MessageEvent += (api, e) =>
+            {
+                Console.WriteLine(e.Content.Text);
+                Console.WriteLine(api is null);
+            };
+            var cancellationTokenSource = new CancellationTokenSource();
+            cqWebSocketEvent.StartListen(cancellationTokenSource.Token);
+            cancellationTokenSource.CancelAfter(TimeSpan.FromMinutes(0.2));
+            Task.Delay(-1).Wait();
+        }
+    }
+}
