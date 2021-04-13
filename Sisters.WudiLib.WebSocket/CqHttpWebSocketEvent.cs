d8be64a516aa92a618949ea2de81d2594ff56716
bleatingsheep <bleatingsheep@live.cn>
2021-04-13 15:30:59 +0900

    Refactoring: let ws event use positive ws manager.

--- Sisters.WudiLib.WebSocket/CqHttpWebSocketEvent.cs
+++ Sisters.WudiLib.WebSocket/CqHttpWebSocketEvent.cs
@@ -14,17 +14,12 @@ namespace Sisters.WudiLib.WebSocket
     /// <summary>
     /// 事件上报的 WebSocket 客户端。请注意，WebSocket 客户端暂不支持直接通过返回值响应。
     /// </summary>
-    public class CqHttpWebSocketEvent : ApiPostListener
+    public class CqHttpWebSocketEvent : ApiPostListener, IDisposable
     {
-        private readonly string _accessToken;
-        private CancellationToken _cancellationToken;
-        private readonly object _listenLock = new object();
-        private Task _listenTask;
-
         /// <summary>
         /// 当前连接的 WebSocket 客户端。如果发生断线重连，则可能改变。
         /// </summary>
-        protected System.Net.WebSockets.WebSocket WebSocket { get; private set; }
+        protected System.Net.WebSockets.WebSocket WebSocket => _manager.WebSocket;
 
         /// <summary>
         /// 引发 <see cref="NotSupportedException"/>。
@@ -44,18 +39,19 @@ namespace Sisters.WudiLib.WebSocket
         /// <summary>
         /// 指示当前是否已启动监听。若要检查当前是否可用，请使用 <see cref="IsAvailable"/> 属性。
         /// </summary>
-        public override bool IsListening => _listenTask?.IsCompleted == false;
+        public override bool IsListening => _manager.IsRunning;
 
         /// <summary>
         /// 获取当前是否能收到上报事件。注意自动重连过程中此项为 <c>false</c>，但无法再次通过 <see cref="StartListen()"/> 或 <see cref="StartListen(CancellationToken)"/> 连接。
         /// </summary>
-        public virtual bool IsAvailable => WebSocket?.State == WebSocketState.Open;
+        public virtual bool IsAvailable => _manager.IsAvailable;
 
         /// <summary>
         /// 构造通过 WebSocket 获取上报的监听客户端。
         /// </summary>
         /// <param name="uri">以 <c>ws://</c> 或者 <c>wss://</c> 开头的 uri，用于连接 WebSocket。</param>
-        public CqHttpWebSocketEvent(string uri) => Uri = uri;
+        public CqHttpWebSocketEvent(string uri) : this(uri, string.Empty)
+        { }
 
         /// <summary>
         /// 构造通过 WebSocket 获取上报的监听客户端。
@@ -63,72 +59,28 @@ namespace Sisters.WudiLib.WebSocket
         /// <param name="uri">以 <c>ws://</c> 或者 <c>wss://</c> 开头的 uri，用于连接 WebSocket。</param>
         /// <param name="accessToken">Access Token.</param>
         public CqHttpWebSocketEvent(string uri, string accessToken)
-            : this(uri) => _accessToken = accessToken;
+        {
+            _manager = new PositiveWebSocketManager(uri, accessToken)
+            {
+                OnMessage = bytes => Task.Run(() => ProcessWSMessageAsync(bytes)),
+                AutoReconnect = true,
+            };
+        }
 
         /// <summary>
         /// 开始从 WebSocket 监听上报。
         /// </summary>
         /// <exception cref="Exception">连接失败等。</exception>
         public override void StartListen()
-            => StartListen(default(CancellationToken));
+            => _ = StartListen(default(CancellationToken));
 
         /// <summary>
         /// 开始从 WebSocket 监听上报。
         /// </summary>
         /// <param name="cancellationToken">一个 <see cref="CancellationToken"/> 应该被使用，以通知此操作应被取消。</param>
         /// <exception cref="Exception">连接失败等。</exception>
-        public void StartListen(CancellationToken cancellationToken)
-        {
-            lock (_listenLock)
-            {
-                if (WebSocket != null && !_cancellationToken.IsCancellationRequested)
-                {
-                    throw new InvalidOperationException("已经有监听任务在执行！");
-                }
-                _cancellationToken = cancellationToken;
-                InitializeWebSocket(cancellationToken);
-            }
-            _listenTask = Listening(cancellationToken);
-        }
-
-        private async Task Listening(CancellationToken cancellationToken)
-        {
-            byte[] buffer = new byte[1024];
-            var ms = new MemoryStream();
-            while (true)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-                byte[] eventArray;
-                try
-                {
-                    var receiveResult = await WebSocket.ReceiveAsync(buffer, cancellationToken).ConfigureAwait(false);
-                    ms.Write(buffer, 0, receiveResult.Count);
-                    if (!receiveResult.EndOfMessage) continue;
-                    eventArray = ms.ToArray();
-                }
-                catch (Exception)
-                {
-                    cancellationToken.ThrowIfCancellationRequested();
-                    try
-                    {
-                        if (WebSocket?.State != WebSocketState.Open)
-                        {
-                            (WebSocket as IDisposable).Dispose();
-                            InitializeWebSocket(cancellationToken);
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                    }
-                    ms = new MemoryStream();
-                    continue;
-                }
-
-                _ = Task.Run(() => ProcessWSMessageAsync(eventArray));
-                ms = new MemoryStream();
-            }
-        }
+        public async Task StartListen(CancellationToken cancellationToken)
+            => await _manager.ConnectAsync(cancellationToken).ConfigureAwait(false);
 
         private async Task ProcessWSMessageAsync(byte[] eventArray)
         {
@@ -165,11 +117,18 @@ namespace Sisters.WudiLib.WebSocket
             }
         }
 
-        private void InitializeWebSocket(CancellationToken cancellationToken)
+        #region Manage WebSocket
+        private readonly CancellationTokenSource _disposeSource = new();
+        private readonly PositiveWebSocketManager _manager;
+
+        /// <summary>
+        /// Disconnects from remote and disposes this object.
+        /// </summary>
+        public void Dispose()
         {
-            cancellationToken.ThrowIfCancellationRequested();
-            ClientWebSocket clientWebSocket = CreateWebSocket(CreateUri(Uri, _accessToken), cancellationToken).GetAwaiter().GetResult();
-            WebSocket = clientWebSocket;
+            _disposeSource.Cancel();
+            _manager.Dispose();
         }
+        #endregion
     }
 }
