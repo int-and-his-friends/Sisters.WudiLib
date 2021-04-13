111e8856fd48a2f694b1dab5ce05a9d02b5f8c56
bleatingsheep <bleatingsheep@live.cn>
2021-04-13 16:06:39 +0900

    Add docs.

--- Sisters.WudiLib.WebSocket/CqHttpWebSocketApiClient.cs
+++ Sisters.WudiLib.WebSocket/CqHttpWebSocketApiClient.cs
@@ -12,6 +12,9 @@ using static Sisters.WudiLib.WebSocket.WebSocketUtility;
 
 namespace Sisters.WudiLib.WebSocket
 {
+    /// <summary>
+    /// 实现通过正向 WebSocket 访问 OneBot API 的类。
+    /// </summary>
     public class CqHttpWebSocketApiClient : HttpApiClient, IDisposable
     {
         private static readonly JsonSerializerSettings s_jsonSerializerSettings = new JsonSerializerSettings();
@@ -24,6 +27,9 @@ namespace Sisters.WudiLib.WebSocket
             return span[0];
         }
 
+        /// <summary>
+        /// 初始化实例，可以被子类调用。
+        /// </summary>
         protected CqHttpWebSocketApiClient() : base("http://wsdefault/")
         {
             _manager = new PositiveWebSocketManager(() => CreateUri(Uri, AccessToken))
@@ -43,8 +49,17 @@ namespace Sisters.WudiLib.WebSocket
             };
         }
 
+        /// <summary>
+        /// 从给定 WebSocket URL 创建实例。
+        /// </summary>
+        /// <param name="uri">正向 WS 监听地址（以 ws:// 或 wss:// 开头）。</param>
         public CqHttpWebSocketApiClient(string uri) : this() => Uri = uri;
 
+        /// <summary>
+        /// 从给定 WebSocket URL 和 Access Token 创建实例。
+        /// </summary>
+        /// <param name="uri">正向 WS 监听地址（以 ws:// 或 wss:// 开头）。</param>
+        /// <param name="accessToken">Access Token。</param>
         public CqHttpWebSocketApiClient(string uri, string accessToken)
             : this(uri) => AccessToken = accessToken;
 
@@ -62,10 +77,17 @@ namespace Sisters.WudiLib.WebSocket
             set => throw new InvalidOperationException("基于正向 WebSocket 的客户端无法在运行时改变连接地址。");
         }
 
+        /// <summary>
+        /// 获取或设置 AccessToken。将在下次自动重连时生效。
+        /// </summary>
         public override string AccessToken { get; set; }
 
         #region Echo
         private int _currentEcho = GetRandomInt32();
+        /// <summary>
+        /// 获取下一个可用的 Echo 值。
+        /// </summary>
+        /// <returns></returns>
         protected int GetNextEcho() => Interlocked.Increment(ref _currentEcho);
         #endregion
 
@@ -74,6 +96,9 @@ namespace Sisters.WudiLib.WebSocket
         private TimeSpan _timeOut = TimeSpan.FromMinutes(1);
         private CancellationTokenSource _failedSource = new CancellationTokenSource();
 
+        /// <summary>
+        /// 等待 API 响应的超时值。
+        /// </summary>
         public TimeSpan TimeOut
         {
             get => _timeOut;
@@ -112,6 +137,12 @@ namespace Sisters.WudiLib.WebSocket
             }
         }
 
+        /// <summary>
+        /// 用未包装的方式调用 API。
+        /// </summary>
+        /// <param name="action">操作。</param>
+        /// <param name="data">参数数据。</param>
+        /// <returns>响应结果。</returns>
         protected override async Task<JObject> CallRawJObjectAsync(string action, object data)
         {
             var jObject = new JObject();
@@ -143,12 +174,21 @@ namespace Sisters.WudiLib.WebSocket
         private readonly CancellationTokenSource _disposeSource = new();
         private readonly PositiveWebSocketManager _manager;
 
+        /// <summary>
+        /// 发送调用消息，被 <see cref="CallRawJObjectAsync(string, object)"/> 调用
+        /// </summary>
+        /// <param name="action">操作。</param>
+        /// <param name="json">参数 json。</param>
+        /// <returns>由于无法直接获取响应，始终为空字符串。</returns>
         protected override async Task<string> CallRawAsync(string action, string json)
         {
             await _manager.SendAsync(Encoding.UTF8.GetBytes(json), WebSocketMessageType.Text, true, _disposeSource.Token).ConfigureAwait(false);
             return string.Empty;
         }
 
+        /// <summary>
+        /// 析构此对象。
+        /// </summary>
         public void Dispose()
         {
             _disposeSource.Cancel();
