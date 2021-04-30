10ec164ef881f0add00e3eb66e83afe882891b57
bleatingsheep <bleatingsheep@live.cn>
2021-05-01 00:15:31 +0900

    Add NegativeWebSocketManager.

---
+++ Sisters.WudiLib.WebSocket/Reverse/NegativeWebSocketManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sisters.WudiLib.WebSocket.Reverse
+{
+    /// <summary>
+    /// 处理单个 WebSocket 连接的被动连接管理器。
+    /// </summary>
+    internal class NegativeWebSocketManager : WebSocketManager, IDisposable
+    {
+        private readonly object _startLock = new object();
+
+        public NegativeWebSocketManager(System.Net.WebSockets.WebSocket webSocket)
+            => WebSocket = webSocket;
+
+        public void Start(CancellationToken cancellationToken)
+        {
+            lock (_startLock)
+            {
+                _listenTask = _listenTask is null
+                    ? RunListeningTask(cancellationToken)
+                    : throw new InvalidOperationException("已经开始执行。");
+            }
+        }
+
+        protected override Task<System.Net.WebSockets.WebSocket> GetWebSocketAsync(CancellationToken cancellationToken)
+            => Task.FromResult(WebSocket);
+
+        protected async override Task RunListeningTask(CancellationToken cancellationToken)
+        {
+            byte[] buffer = new byte[1024];
+            var ms = new MemoryStream();
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                byte[] eventArray;
+                try
+                {
+                    var receiveResult = await WebSocket.ReceiveAsync(buffer, cancellationToken).ConfigureAwait(false);
+                    ms.Write(buffer, 0, receiveResult.Count);
+                    if (!receiveResult.EndOfMessage)
+                        continue;
+                    eventArray = ms.ToArray();
+                }
+                catch (Exception)
+                {
+                    if (!IsAvailable)
+                    {
+                        // 当出现异常后确认了不可用，被动管理无法重连。
+                        // 回收资源，然后退出。
+                        var ws = WebSocket;
+                        WebSocket = null;
+                        (ws as IDisposable)?.Dispose();
+                        OnSocketDisconnected?.Invoke();
+                        break;
+                    }
+                    ms = new MemoryStream();
+                    continue;
+                }
+
+                try
+                {
+                    OnMessage?.Invoke(eventArray);
+                }
+#pragma warning disable RCS1075 // Avoid empty catch clause that catches System.Exception.
+                catch (Exception)
+#pragma warning restore RCS1075 // Avoid empty catch clause that catches System.Exception.
+                {
+                    // ignored
+                }
+                ms = new MemoryStream();
+            }
+        }
+
+        public void Dispose() => WebSocket?.Dispose();
+    }
+}
