445c28bfbfe0810746929079adf85f526c2605dd
bleatingsheep <bleatingsheep@live.cn>
2021-09-26 01:15:41 +0800

    Fix: blocking of reverse WS when request event.

--- Sisters.WudiLib.WebSocket/WebSocketEventExtensions.cs
+++ Sisters.WudiLib.WebSocket/WebSocketEventExtensions.cs
@@ -8,6 +8,7 @@ namespace Sisters.WudiLib.WebSocket
     {
         internal static async Task ProcessWSMessageAsync(this ApiPostListener listener, JObject eventObject)
         {
+            await Task.Yield();
             var response = listener.ProcessPost(eventObject);
             var apiClient = listener.ApiClient;
             if (response is RequestResponse && !(apiClient is null))
